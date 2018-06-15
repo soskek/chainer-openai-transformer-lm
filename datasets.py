248c17ff66a8ad92d1afcfa4c3a9e7e3f2e738f8
Sosuke Kobayashi <sosk@preferred.jp>
2018-06-16 07:58:18 +0900

    autopep8

--- datasets.py
+++ datasets.py
@@ -9,6 +9,7 @@ from sklearn.model_selection import train_test_split
 
 seed = 3535999445
 
+
 def _rocstories(path):
     with open(path) as f:
         f = csv.reader(f)
@@ -24,13 +25,17 @@ def _rocstories(path):
                 st.append(s)
                 ct1.append(c1)
                 ct2.append(c2)
-                y.append(int(line[-1])-1)
+                y.append(int(line[-1]) - 1)
         return st, ct1, ct2, y
 
+
 def rocstories(data_dir, n_train=1497, n_valid=374):
-    storys, comps1, comps2, ys = _rocstories(os.path.join(data_dir, 'cloze_test_val__spring2016 - cloze_test_ALL_val.csv'))
-    teX1, teX2, teX3, _ = _rocstories(os.path.join(data_dir, 'cloze_test_test__spring2016 - cloze_test_ALL_test.csv'))
-    tr_storys, va_storys, tr_comps1, va_comps1, tr_comps2, va_comps2, tr_ys, va_ys = train_test_split(storys, comps1, comps2, ys, test_size=n_valid, random_state=seed)
+    storys, comps1, comps2, ys = _rocstories(os.path.join(
+        data_dir, 'cloze_test_val__spring2016 - cloze_test_ALL_val.csv'))
+    teX1, teX2, teX3, _ = _rocstories(os.path.join(
+        data_dir, 'cloze_test_test__spring2016 - cloze_test_ALL_test.csv'))
+    tr_storys, va_storys, tr_comps1, va_comps1, tr_comps2, va_comps2, tr_ys, va_ys = train_test_split(
+        storys, comps1, comps2, ys, test_size=n_valid, random_state=seed)
     trX1, trX2, trX3 = [], [], []
     trY = []
     for s, c1, c2, y in zip(tr_storys, tr_comps1, tr_comps2, tr_ys):
--- opt.py
+++ opt.py
@@ -3,28 +3,33 @@ import torch
 from torch.optim import Optimizer
 from torch.nn.utils import clip_grad_norm_
 
+
 def warmup_cosine(x, warmup=0.002):
     s = 1 if x <= warmup else 0
-    return s*(x/warmup) + (1-s)*(0.5 * (1 + torch.cos(math.pi * x)))
+    return s * (x / warmup) + (1 - s) * (0.5 * (1 + torch.cos(math.pi * x)))
+
 
 def warmup_constant(x, warmup=0.002):
     s = 1 if x <= warmup else 0
-    return s*(x/warmup) + (1-s)*1
+    return s * (x / warmup) + (1 - s) * 1
+
 
 def warmup_linear(x, warmup=0.002):
     s = 1 if x <= warmup else 0
-    return (s*(x/warmup) + (1-s))*(1-x)
+    return (s * (x / warmup) + (1 - s)) * (1 - x)
+
 
 SCHEDULES = {
-    'warmup_cosine':warmup_cosine,
-    'warmup_constant':warmup_constant,
-    'warmup_linear':warmup_linear,
+    'warmup_cosine': warmup_cosine,
+    'warmup_constant': warmup_constant,
+    'warmup_linear': warmup_linear,
 }
 
 
 class OpenAIAdam(Optimizer):
     """Implements Open AI version of Adam algorithm with weight decay fix.
     """
+
     def __init__(self, params, lr, schedule, warmup, t_total,
                  b1=0.9, b2=0.999, e=1e-8, l2=0,
                  vector_l2=False, max_grad_norm=-1, **kwargs):
@@ -40,9 +45,17 @@ class OpenAIAdam(Optimizer):
             raise ValueError("Invalid b2 parameter: {}".format(b2))
         if not 0.0 <= e:
             raise ValueError("Invalid epsilon value: {}".format(e))
-        defaults = dict(lr=lr, schedule=schedule, warmup=warmup, t_total=t_total,
-                        b1=b1, b2=b2, e=e, l2=l2, vector_l2=vector_l2,
-                        max_grad_norm=max_grad_norm)
+        defaults = dict(
+            lr=lr,
+            schedule=schedule,
+            warmup=warmup,
+            t_total=t_total,
+            b1=b1,
+            b2=b2,
+            e=e,
+            l2=l2,
+            vector_l2=vector_l2,
+            max_grad_norm=max_grad_norm)
         super(OpenAIAdam, self).__init__(params, defaults)
 
     def step(self, closure=None):
@@ -62,7 +75,8 @@ class OpenAIAdam(Optimizer):
                     continue
                 grad = p.grad.data
                 if grad.is_sparse:
-                    raise RuntimeError('Adam does not support sparse gradients, please consider SparseAdam instead')
+                    raise RuntimeError(
+                        'Adam does not support sparse gradients, please consider SparseAdam instead')
 
                 state = self.state[p]
 
@@ -92,13 +106,16 @@ class OpenAIAdam(Optimizer):
                 bias_correction2 = 1 - beta2 ** state['step']
 
                 schedule_fct = SCHEDULES[group['schedule']]
-                lr_scheduled = group['lr'] * schedule_fct(state['step']/group['t_total'], group['warmup'])
-                step_size = lr_scheduled * math.sqrt(bias_correction2) / bias_correction1
+                lr_scheduled = group['lr'] * schedule_fct(
+                    state['step'] / group['t_total'], group['warmup'])
+                step_size = lr_scheduled * \
+                    math.sqrt(bias_correction2) / bias_correction1
 
                 p.data.addcdiv_(-step_size, exp_avg, denom)
 
                 # Add weight decay at the end (fixed version)
-                if (len(p.size()) > 1 or group['vector_l2']) and group['l2'] > 0:
+                if (len(p.size()) >
+                        1 or group['vector_l2']) and group['l2'] > 0:
                     p.data.add_(-lr_scheduled * group['l2'], p.data)
 
         return loss
